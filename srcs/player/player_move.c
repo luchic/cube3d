8350164a34af1b620136742c55df27d4ec15898d
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-30 14:22:25 +0200

    Fix playe collisions

--- srcs/player/player_move.c
+++ srcs/player/player_move.c
@@ -12,6 +12,14 @@
 
 #include "cub3d.h"
 
+static int32_t	has_wall_padding(t_app *app, double x, double y)
+{
+	return (is_wall(app, x - PLAYER_RADIUS, y - PLAYER_RADIUS)
+		|| is_wall(app, x + PLAYER_RADIUS, y - PLAYER_RADIUS)
+		|| is_wall(app, x - PLAYER_RADIUS, y + PLAYER_RADIUS)
+		|| is_wall(app, x + PLAYER_RADIUS, y + PLAYER_RADIUS));
+}
+
 void	move_forward_backward(t_app *app, double move_dir)
 {
 	double	new_pos_x;
@@ -21,9 +29,9 @@ void	move_forward_backward(t_app *app, double move_dir)
 	move_speed = MOVE_SPEED;
 	new_pos_x = app->player.pos_x + app->player.dir_x * move_speed * move_dir;
 	new_pos_y = app->player.pos_y + app->player.dir_y * move_speed * move_dir;
-	if (!is_wall(app, new_pos_x, app->player.pos_y))
+	if (!has_wall_padding(app, new_pos_x, app->player.pos_y))
 		app->player.pos_x = new_pos_x;
-	if (!is_wall(app, app->player.pos_x, new_pos_y))
+	if (!has_wall_padding(app, app->player.pos_x, new_pos_y))
 		app->player.pos_y = new_pos_y;
 }
 
@@ -38,8 +46,8 @@ void	move_strafe(t_app *app, double strafe_dir)
 		* strafe_dir;
 	new_pos_y = app->player.pos_y + app->player.plane_y * move_speed
 		* strafe_dir;
-	if (!is_wall(app, new_pos_x, app->player.pos_y))
+	if (!has_wall_padding(app, new_pos_x, app->player.pos_y))
 		app->player.pos_x = new_pos_x;
-	if (!is_wall(app, app->player.pos_x, new_pos_y))
+	if (!has_wall_padding(app, app->player.pos_x, new_pos_y))
 		app->player.pos_y = new_pos_y;
 }
