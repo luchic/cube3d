88fba7f22ba62412ffc09cf3708d3904c483806f
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-04-18 20:40:34 +0200

    Add drow textures

--- srcs/render/render_frame.c
+++ srcs/render/render_frame.c
@@ -1,10 +1,6 @@
 #include "cub3d.h"
 #include <stdio.h>
 
-static int32_t	__get_rgba(int32_t r, int32_t g, int32_t b, int32_t a)
-{
-	return ((uint32_t)r << 24 | (uint32_t)g << 16 | (uint32_t)b << 8 | (uint32_t)a);
-}
 
 t_casting_info	start_casting(t_app *app, int x)
 {
@@ -22,11 +18,45 @@ t_casting_info	start_casting(t_app *app, int x)
 	return (info);
 }
 
-static void	drow_line_cast(t_app *app, int pixel, int x)
+// static void	drow_line_cast(t_app *app, int pixel, int x)
+// {
+// 	int32_t		y;
+// 	int32_t		y_end;
+// 	uint32_t	color;
+
+// 	if (pixel < app->window_height)
+// 	{
+// 		y = app->window_height / 2 - pixel / 2;
+// 		y_end = app->window_height / 2 + pixel / 2;
+// 	}
+// 	else
+// 	{
+// 		y = 0;
+// 		y_end = app->window_height;
+// 	}
+// 	while (y < y_end)
+// 	{
+// 		color = get_rgba(0, 255, 0, 255);
+// 		mlx_put_pixel(app->img->screen, x, y, color);
+// 		y++;
+// 	}
+// }
+
+static int32_t __get_texture_color(t_app *app, int cur, int max_height, int column)
+{
+	double tmp = (double)cur / (double) max_height;
+	double tmp_y = tmp * 64.0;
+	int tex_y = tmp_y;
+
+	return (get_pixel(app->img->txt_no, column, tex_y));
+}
+
+static void	drow_line_from_texture(t_app *app, int pixel, int x, int text_pos)
 {
 	int32_t		y;
 	int32_t		y_end;
 	uint32_t	color;
+	int tex_y;
 
 	if (pixel < app->window_height)
 	{
@@ -38,20 +68,42 @@ static void	drow_line_cast(t_app *app, int pixel, int x)
 		y = 0;
 		y_end = app->window_height;
 	}
+	tex_y = 0;
 	while (y < y_end)
 	{
-		color = __get_rgba(0, 255, 0, 255);
+		color = __get_texture_color(app, tex_y, pixel, text_pos);
 		mlx_put_pixel(app->img->screen, x, y, color);
 		y++;
+		tex_y++;
 	}
+}
+
+static int get_tex_x(t_casting_info info)
+{
+	double	wall_x;
+	int		tex_x;
+	int texture_width = 64;
+
+	if (info.side == 0)
+		wall_x = info.hit_position.y;
+	else
+		wall_x = info.hit_position.x;
+	wall_x -= floor(wall_x);
+	tex_x = (int)(wall_x * texture_width);
+	if (tex_x < 0)
+		tex_x = 0;
+	if (tex_x >= texture_width)
+		tex_x = texture_width - 1;
+	return (tex_x);
 }
 
 void	draw_wertical_line(t_app *app, t_casting_info info, int x)
 {
 	double	height;
-
+	int texture_column;
 	height = app->player.len_to_screen / info.distanse * 64.0;
-	drow_line_cast(app, (int)height, x);
+	texture_column = get_tex_x(info);
+	drow_line_from_texture(app, (int)height, x, texture_column);
 }
 
 void	render_walls(t_app *app)
@@ -63,7 +115,7 @@ void	render_walls(t_app *app)
 	while (x < app->window_width)
 	{
 		info = start_casting(app, x);
-		if (info.hit_gorisontal)
+		if (info.hit)
 		{
 			draw_wertical_line(app, info, x);
 		}
@@ -79,6 +131,8 @@ void	re_draw(t_app *app)
 		return ;
 	ceiling_color = get_rgba(app->map->ceiling_color[0],
 			app->map->ceiling_color[1], app->map->ceiling_color[2], 255);
+	// I don't need to recalculate it all the time. I need just another one layer
+	// For my sky
 	draw_sky(app, ceiling_color);
 	draw_floor(app);
 	render_walls(app);
