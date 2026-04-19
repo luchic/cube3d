15793d6d1a6d78b9db8fef72fc93dbaa914b3f08
Stefan Ruff <sruff@2-E-9.42heilbronn.de>
2026-04-19 18:16:36 +0200

    validation rework

--- srcs/parsing/validate_map.c
+++ srcs/parsing/validate_map.c
@@ -6,83 +6,88 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 12:20:00 by sruff             #+#    #+#             */
-/*   Updated: 2026/04/19 18:01:20 by sruff            ###   ########.fr       */
+/*   Updated: 2026/04/19 18:15:41 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-
-static t_parse_error	validate_character(t_app *app, int32_t x, int32_t y,
-		int32_t *player_count)
+static t_parse_error	validate_textures(t_map *map)
 {
-	char	c;
-	t_map	*map;
-
-	map = app->map;
-	c = map->grid[y][x];
-	if (c == 'N' || c == 'S' || c == 'E' || c == 'W')
-	{
-		if (*player_count > 0)
-			return (PARSE_ERR_INVALID_PLAYER_COUNT);
-		map->player_start_x = x;
-		map->player_start_y = y;
-		map->player_start_dir = c;
-		(*player_count)++;
-	}
-	else if (c != '0' && c != '1' && c != ' ')
-		return (PARSE_ERR_INVALID_MAP_CHAR);
+	if (!validate_texture_file(map->north_texture_path))
+		return (PARSE_ERR_TEXTURE_FILE);
+	if (!validate_texture_file(map->south_texture_path))
+		return (PARSE_ERR_TEXTURE_FILE);
+	if (!validate_texture_file(map->west_texture_path))
+		return (PARSE_ERR_TEXTURE_FILE);
+	if (!validate_texture_file(map->east_texture_path))
+		return (PARSE_ERR_TEXTURE_FILE);
 	return (PARSE_SUCCESS);
 }
 
-static t_parse_error	check_map_characters(t_app *app)
+static t_parse_error	check_enclosed(t_map *map)
 {
-	int32_t			player_count;
-	int32_t			y;
-	int32_t			x;
-	t_map			*map;
-	t_parse_error	error;
+	int	x;
+	int	y;
 
-	map = app->map;
-	player_count = 0;
 	y = -1;
 	while (++y < map->grid_height)
 	{
 		x = -1;
 		while (++x < map->grid_width)
 		{
-			error = validate_character(app, x, y, &player_count);
-			if (error != PARSE_SUCCESS)
-				return (error);
+			if (ft_strchr("0NSEW", map->grid[y][x]))
+			{
+				if (x == 0 || x == map->grid_width - 1
+					|| y == 0 || y == map->grid_height - 1)
+					return (PARSE_ERR_MAP_NOT_ENCLOSED);
+				if (map->grid[y][x + 1] == ' ' || map->grid[y][x - 1] == ' '
+					|| map->grid[y + 1][x] == ' ' || map->grid[y - 1][x] == ' ')
+					return (PARSE_ERR_MAP_NOT_ENCLOSED);
+			}
 		}
 	}
-	if (player_count != 1)
-		return (PARSE_ERR_INVALID_PLAYER_COUNT);
 	return (PARSE_SUCCESS);
 }
 
-static t_parse_error	check_elements_present(t_app *app)
+static t_parse_error	check_map_chars(t_map *map, int *players)
 {
-	int32_t	i;
+	int	x;
+	int	y;
 
-	i = -1;
-	while (++i < ELEMENT_COUNT)
+	y = -1;
+	while (++y < map->grid_height)
 	{
-		if (!app->map->elements_found[i])
-			return (PARSE_ERR_MISSING_ELEMENTS);
+		x = -1;
+		while (++x < map->grid_width)
+		{
+			if (!ft_strchr("01NSEW ", map->grid[y][x]))
+				return (PARSE_ERR_INVALID_MAP_CHAR);
+			if (ft_strchr("NSEW", map->grid[y][x]))
+			{
+				(*players)++;
+				map->player_start_x = x;
+				map->player_start_y = y;
+				map->player_start_dir = map->grid[y][x];
+			}
+		}
 	}
 	return (PARSE_SUCCESS);
 }
 
-t_parse_error	validate_map(t_app *app)
+t_parse_error	validate_map(t_map *map)
 {
-	t_parse_error	error;
+	int				players;
+	t_parse_error	err;
 
-	error = check_elements_present(app);
-	if (error != PARSE_SUCCESS)
-		return (error);
-	error = check_map_characters(app);
-	if (error != PARSE_SUCCESS)
-		return (error);
-	return (check_walls_enclosed(app));
+	players = 0;
+	err = check_map_chars(map, &players);
+	if (err != PARSE_SUCCESS)
+		return (err);
+	if (players != 1)
+		return (PARSE_ERR_INVALID_PLAYER_COUNT);
+	err = validate_textures(map);
+	if (err != PARSE_SUCCESS)
+		return (err);
+	return (check_enclosed(map));
 }
