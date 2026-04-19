f8dbedfb38a102f67ff71ddeb133d04b05630b00
Stefan Ruff <sruff@2-E-9.42heilbronn.de>
2026-04-19 18:23:17 +0200

    less ** handling/conversion changed memory management

--- srcs/parsing/parse_grid.c
+++ srcs/parsing/parse_grid.c
@@ -6,80 +6,87 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 12:10:00 by sruff             #+#    #+#             */
-/*   Updated: 2026/04/17 12:10:00 by sruff            ###   ########.fr       */
+/*   Updated: 2026/04/19 18:20:03 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static t_list	*read_grid_to_list(int fd, char *first_line)
+static t_parse_error	add_line_to_grid(t_map *map, char *line)
 {
-	t_list	*list;
-	char	*line;
+	char	**new_grid;
+	int		i;
+	int		len;
 
-	list = ft_lstnew(malloc_strdup(first_line));
-	if (!list)
-		return (NULL);
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		if (line[ft_strlen(line) - 1] == '\n')
-			line[ft_strlen(line) - 1] = '\0';
-		ft_lstadd_back(&list, ft_lstnew(malloc_strdup(line)));
-		free(line);
-	}
-	return (list);
+	new_grid = ft_malloc(sizeof(char *) * (map->grid_height + 2));
+	if (!new_grid)
+		return (PARSE_ERR_ALLOC);
+	i = -1;
+	while (++i < map->grid_height)
+		new_grid[i] = map->grid[i];
+	len = ft_strlen(line);
+	if (len > 0 && line[len - 1] == '\n')
+		line[--len] = '\0';
+	new_grid[i] = malloc_strdup(line);
+	if (!new_grid[i])
+		return (PARSE_ERR_ALLOC);
+	new_grid[i + 1] = NULL;
+	map->grid = new_grid;
+	map->grid_height++;
+	if (len > map->grid_width)
+		map->grid_width = len;
+	return (PARSE_SUCCESS);
 }
 
-static char	**convert_list_to_array(t_list *list, int32_t *height, int32_t *width)
+t_parse_error	pad_grid(t_map *map)
 {
-	char	**grid;
-	int32_t	h;
-	int32_t	w;
-	t_list	*tmp;
+	int		i;
+	char	*new_line;
+	int		len;
 
-	h = ft_lstsize(list);
-	grid = ft_calloc(h + 1, sizeof(char *));
-	if (!grid)
-		return (NULL);
-	w = 0;
-	tmp = list;
-	h = 0;
-	while (tmp)
+	i = -1;
+	while (++i < map->grid_height)
 	{
-		grid[h] = (char *)tmp->content;
-		if ((int32_t)ft_strlen(grid[h]) > w)
-			w = ft_strlen(grid[h]);
-		tmp = tmp->next;
-		h++;
+		len = ft_strlen(map->grid[i]);
+		if (len < map->grid_width)
+		{
+			new_line = ft_malloc(map->grid_width + 1);
+			if (!new_line)
+				return (PARSE_ERR_ALLOC);
+			ft_memset(new_line, ' ', map->grid_width);
+			ft_memcpy(new_line, map->grid[i], len);
+			new_line[map->grid_width] = '\0';
+			map->grid[i] = new_line;
+		}
 	}
-	*height = h;
-	*width = w;
-	return (grid);
+	return (PARSE_SUCCESS);
 }
 
 t_parse_error	parse_grid(t_parse_ctx *ctx, char *first_line)
 {
-	t_list	*list;
+	char			*line;
+	t_parse_error	err;
+	bool			empty_line_found;
 
-	list = read_grid_to_list(ctx->fd, first_line);
-	if (!list)
-		return (PARSE_ERR_ALLOC);
-	ctx->map->grid = convert_list_to_array(list, &ctx->map->grid_height, &ctx->map->grid_width);
-	// We don't free the content because we moved pointers to the array
-	// But we should free the list nodes. 
-	// However, simple ft_lstclear would free content if we pass free.
-	// We need a clear that doesn't free content.
-	t_list *tmp;
-	while (list)
+	err = add_line_to_grid(ctx->map, first_line);
+	if (err != PARSE_SUCCESS)
+		return (err);
+	empty_line_found = false;
+	while (1)
 	{
-		tmp = list;
-		list = list->next;
-		free(tmp);
+		line = get_next_line(ctx->fd);
+		if (!line)
+			break ;
+		if (is_empty_line(line))
+			empty_line_found = true;
+		else
+		{
+			if (empty_line_found)
+				return (PARSE_ERR_EMPTY_LINE_IN_MAP);
+			err = add_line_to_grid(ctx->map, line);
+			if (err != PARSE_SUCCESS)
+				return (err);
+		}
 	}
-	if (!ctx->map->grid)
-		return (PARSE_ERR_ALLOC);
-	return (PARSE_SUCCESS);
+	return (pad_grid(ctx->map));
 }
