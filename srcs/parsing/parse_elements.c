b1a70008b1611cf565602da574539714d4a8bd50
Stefan Ruff <sruff@2-E-9.42heilbronn.de>
2026-04-19 18:33:39 +0200

    better error management. added util digit_string fn. upgraded parsing logic

--- srcs/parsing/parse_elements.c
+++ srcs/parsing/parse_elements.c
@@ -6,84 +6,98 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 12:00:00 by sruff             #+#    #+#             */
-/*   Updated: 2026/04/17 12:00:00 by sruff            ###   ########.fr       */
+/*   Updated: 2026/04/19 18:31:11 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static int	parse_rgb(char *str, int32_t *color)
+static t_parse_error	parse_texture(char **split, char **path, uint8_t bit,
+		t_parse_ctx *ctx)
 {
-	char	**split;
-	int		i;
-
-	split = ft_split(str, ',');
-	if (!split)
-		return (0);
-	i = 0;
-	while (split[i])
-		i++;
-	if (i != 3)
-	{
-		// ft_free_split(split);
-		return (0);
-	}
-	color[0] = ft_atoi(split[0]);
-	color[1] = ft_atoi(split[1]);
-	color[2] = ft_atoi(split[2]);
-	// ft_free_split(split);
-	if (color[0] < 0 || color[0] > 255 || color[1] < 0 || color[1] > 255
-		|| color[2] < 0 || color[2] > 255)
-		return (0);
-	return (1);
-}
-
-static t_parse_error	set_texture(char **path_ptr, char *line, uint8_t *found, uint8_t bit)
-{
-	if (*found & bit)
+	if (ctx->elements_found & bit)
 		return (PARSE_ERR_DUPLICATE_TEXTURE);
-	while (*line && ft_isspace(*line))
-		line++;
-	*path_ptr = malloc_strdup(line);
-	if (!*path_ptr)
+	if (!split[1] || split[2])
+		return (PARSE_ERR_INVALID_LINE);
+	*path = malloc_strdup(split[1]);
+	if (!*path)
 		return (PARSE_ERR_ALLOC);
-	if (!validate_texture_file(*path_ptr))
-		return (PARSE_ERR_TEXTURE_FILE);
-	*found |= bit;
+	ctx->elements_found |= bit;
 	return (PARSE_SUCCESS);
 }
 
-static t_parse_error	set_color(int32_t *color_ptr, char *line, uint8_t *found, uint8_t bit)
+static int	is_digit_str(char *str)
 {
-	if (*found & bit)
+	int	i;
+
+	i = 0;
+	if (!str || !str[0])
+		return (0);
+	while (str[i] && ft_isspace(str[i]))
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]) && !ft_isspace(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static t_parse_error	parse_color(char **split, int32_t color[3], uint8_t bit,
+		t_parse_ctx *ctx)
+{
+	char	**rgb;
+	int		i;
+	int		val;
+
+	if (ctx->elements_found & bit)
 		return (PARSE_ERR_DUPLICATE_COLOR);
-	while (*line && ft_isspace(*line))
-		line++;
-	if (!parse_rgb(line, color_ptr))
+	if (!split[1] || split[2])
+		return (PARSE_ERR_INVALID_LINE);
+	rgb = ft_split(split[1], ',');
+	if (!rgb)
+		return (PARSE_ERR_ALLOC);
+	i = -1;
+	while (rgb[++i])
+	{
+		val = ft_atoi(rgb[i]);
+		if (i >= 3 || !is_digit_str(rgb[i]) || val < 0 || val > 255)
+			return (PARSE_ERR_INVALID_RGB);
+		color[i] = val;
+	}
+	if (i != 3)
 		return (PARSE_ERR_INVALID_RGB);
-	*found |= bit;
+	ctx->elements_found |= bit;
 	return (PARSE_SUCCESS);
 }
 
 t_parse_error	parse_element(char *line, t_parse_ctx *ctx)
 {
-	while (*line && ft_isspace(*line))
-		line++;
-	if (!*line)
-		return (PARSE_SUCCESS);
-	if (!ft_strncmp(line, "NO ", 3))
-		return (set_texture(&ctx->map->north_texture_path, line + 3, &ctx->elements_found, ELEM_NO));
-	if (!ft_strncmp(line, "SO ", 3))
-		return (set_texture(&ctx->map->south_texture_path, line + 3, &ctx->elements_found, ELEM_SO));
-	if (!ft_strncmp(line, "WE ", 3))
-		return (set_texture(&ctx->map->west_texture_path, line + 3, &ctx->elements_found, ELEM_WE));
-	if (!ft_strncmp(line, "EA ", 3))
-		return (set_texture(&ctx->map->east_texture_path, line + 3, &ctx->elements_found, ELEM_EA));
-	if (!ft_strncmp(line, "F ", 2))
-		return (set_color(ctx->map->floor_color, line + 2, &ctx->elements_found, ELEM_F));
-	if (!ft_strncmp(line, "C ", 2))
-		return (set_color(ctx->map->ceiling_color, line + 2, &ctx->elements_found, ELEM_C));
-	if (ctx->elements_found == ELEM_ALL)
+	char 			**split;
+	t_parse_error	err;
+
+	if (!line || !*line)
 		return (PARSE_NO_MATCH);
-	return (PARSE_ERR_INVALID_LINE);
+	split = ft_split(line, ' ');
+	if (!split)
+		return (PARSE_ERR_ALLOC);
+	if (!split[0])
+		return (PARSE_NO_MATCH);
+	err = PARSE_NO_MATCH;
+	if (!ft_strcmp(split[0], "NO"))
+		err = parse_texture(split, &ctx->map->north_texture_path, ELEM_NO, ctx);
+	else if (!ft_strcmp(split[0], "SO"))
+		err = parse_texture(split, &ctx->map->south_texture_path, ELEM_SO, ctx);
+	else if (!ft_strcmp(split[0], "WE"))
+		err = parse_texture(split, &ctx->map->west_texture_path, ELEM_WE, ctx);
+	else if (!ft_strcmp(split[0], "EA"))
+		err = parse_texture(split, &ctx->map->east_texture_path, ELEM_EA, ctx);
+	else if (!ft_strcmp(split[0], "F"))
+		err = parse_color(split, ctx->map->floor_color, ELEM_F, ctx);
+	else if (!ft_strcmp(split[0], "C"))
+		err = parse_color(split, ctx->map->ceiling_color, ELEM_C, ctx);
+	return (err);
 }
