25b61a568c74096e28c6f2825b403aa65e242061
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-04-24 21:18:29 +0200

    fix bug with wrong texture side.

--- srcs/render/cast_ray1.c
+++ srcs/render/cast_ray1.c
@@ -5,10 +5,10 @@ t_wall_face	get_wall_face(t_casting_info info)
 	if (info.side == 0)
 	{
 		if (info.direction.x > 0)
-			return (WALL_WEST);
-		return (WALL_EAST);
+			return (WALL_EAST);
+		return (WALL_WEST);
 	}
 	if (info.direction.y > 0)
-		return (WALL_NORTH);
-	return (WALL_SOUTH);
+		return (WALL_SOUTH);
+	return (WALL_NORTH);
 }
